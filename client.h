5c1d0f9425a7dacf280fcd39a6173ae2c107ce31
SoleBridge <100041719+SoleBridge@users.noreply.github.com>
2024-02-14 17:26:33 -0800

    Update client.h

--- client.h
+++ client.h
@@ -1,6 +1,4 @@
 #pragma once
 #include "sockets.h"
-#include <ncurses.h>
 
-int client_main(int argc, char *argv[], char *env[]);                // the 'main' function for the client
 int connect_to_server(const char *SERVER_IP, const int SERVER_PORT); // function for client to connect to a server; returns socket fd, FAILS
