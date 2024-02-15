24e8000ec5c9697b75ffbcac85aa31cde8f01e56
SoleBridge <100041719+SoleBridge@users.noreply.github.com>
2024-02-14 17:27:29 -0800

    Update server.h

--- server.h
+++ server.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "sockets.h"
 
-int server_handle_client(int argc, char *argv[], char *env[], int client_socket_fd, char line[MAX_BUFFER_SIZE]); // the function called when the server deals with a client
-int server_start_listen(int argc, char *argv[], char *env[]);                                                    // the 'main' function for the server
+int server_start(int *server_socket_fd, int *client_socket_fd);                                                                                   // start up the server: create socket, bind, and listen
+int server_accept_client(int *server_socket_fd, int *client_socket_fd, struct sockaddr_in *client_addr);                                          // wait, and connect to client
+int server_handle_client(int argc, char *argv[], char *env[], struct sockaddr_in *client_addr, int client_socket_fd, char line[MAX_BUFFER_SIZE]); // the function called when the server deals with a client
